1042efb2cff2e4f620d4e70d87d47774fd14bffb
Jenny Le <jennyle.dev@gmail.com>
2019-11-01 08:32:48 -0600

    Started November's challenge

---
+++ november-2019/solution.js
@@ -0,0 +1,23 @@
+const volunteers = [
+    'Sally',
+    'Jake',
+    'Brian',
+    'Hamid'
+  ];
+
+
+  const neighbourhoods = [
+    'Central Valley',
+    'Big Mountain',
+    'Little Bridge',
+    'Bricktown',
+    'Brownsville',
+    "Paul's Boutique",
+    'Clay Park',
+    'Fox Nest'
+  ];
+
+  // Challenge #1
+  const doorToDoor = (volunteers, neighbourhoods) => {
+    return (neighbourhoods.length / volunteers.length);
+  }
