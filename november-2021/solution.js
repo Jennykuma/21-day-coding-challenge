4d7e3be792c8973ce1af25513fc25549e551291c
Jenny <jenny.le@ucalgary.ca>
2021-11-23 19:32:57 -0700

    Challenge #2, use string interpolation for #1 and #2

--- november-2021/solution.js
+++ november-2021/solution.js
@@ -1,4 +1,9 @@
 // Challenge #1
 const parseMessage = (origin, message) => {
-  return origin + ": " + message
+  return `${origin}: ${message}`
+}
+
+// Challenge #2
+const generateAstronautTag = (astronaut) => {
+  return `${astronaut.prefix}: ${astronaut.firstName} "${astronaut.nickname}" ${astronaut.lastName}`
 }
