13755200cf54b7cff18b76f02e8d9b31a2a78bac
Jenny <jenny.le@ucalgary.ca>
2021-11-24 19:03:22 -0700

    Add Challenge #3 solution

--- november-2021/solution.js
+++ november-2021/solution.js
@@ -7,3 +7,9 @@ const parseMessage = (origin, message) => {
 const generateAstronautTag = (astronaut) => {
   return `${astronaut.prefix}: ${astronaut.firstName} "${astronaut.nickname}" ${astronaut.lastName}`
 }
+
+// Challenge #3
+
+const checkGaugeStatus = (gauge) => {
+  return gauge.min < gauge.current && gauge.current < gauge.max
+}
